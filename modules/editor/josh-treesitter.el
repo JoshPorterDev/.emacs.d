35b5659ad23f03a73c1ea0f234288dfaf081f3f3
Josh Porter <josh@joshporter.xyz>
2023-08-14 22:38:05 -0500

    Update config to use built in tree sitter with v29

--- modules/editor/josh-treesitter.el
+++ modules/editor/josh-treesitter.el
@@ -9,11 +9,25 @@
 
 ;;; Code:
 
-(use-package tree-sitter
-  :config
-  (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-(use-package tree-sitter-langs)
+;; (use-package tree-sitter
+;;   :config
+;;   (global-tree-sitter-mode)
+;;   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+;; (use-package tree-sitter-langs)
+
+(require 'treesit)
+
+(setq treesit-language-source-alist
+	  '((c "https://github.com/tree-sitter/tree-sitter-c")
+		(cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+		(go "https://github.com/tree-sitter/tree-sitter-go")
+		(gomod "https://github.com/camdencheek/tree-sitter-go-mod")))
+
+(dolist (lang treesit-language-source-alist)
+  (unless (treesit-language-available-p (car lang))
+    (treesit-install-language-grammar (car lang))))
+
+(setq treesit-font-lock-level 4)
 
 (provide 'josh-treesitter)
 
